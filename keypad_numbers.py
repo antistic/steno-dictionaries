33a7946c931c90e1316018c86156324bfaf57431
Antonia Siu <mail@antoniasiu.co.uk>
2022-02-13 10:29:13 +0000

    switch to two digit system (and rewrite with plover-stroke)

--- keypad_numbers.py
+++ keypad_numbers.py
@@ -1,127 +1,125 @@
 #!/usr/bin/python
 """Numbers dictionary
 
-Adapted from: /u/a589
-https://www.reddit.com/r/Plover/comments/fgt6tp/list_of_alternative_number_systems/
+Keypad design inspired by:
+    https://www.reddit.com/r/Plover/comments/fgt6tp/list_of_alternative_number_systems/
+
+    The original design allows for three digits at a time. I rarely used this, and
+    it was hard to use with a number bar (it was designed for thumb number keys).
+    So I've chosen to reduce the digits, and made more room for options. Most of the
+    options use only the top row of the left hand, making it easier to hit with a
+    number bar.
 
 Requirements:
     - python dictionary plugin (https://pypi.org/project/plover-python-dictionary/)
-    - scripts/conflicts.py if you want to find conflicts with other dictionaries
+    - recent version of Plover (after October 2021)
 
 Usage:
-    Enter up to 3 digits at once
+    Enter up to 2 digits at once
 
-    - First digit: PWHR
-    - Second digit: FRPB
-    - Third digit: LGTS
+    Digits are arranged in blocks of four keys:
 
-    The corners are 1, 3, 7, 9.
-    Adjacent keys give you the ones in between.
+        - First digit: FRPB
+        - Second digit: LGTS
 
-    Any diagonal gives 0.
-    Any 3 with the top row gives 00.
-    Any 3 with the bottom row gives 000.
+        The corners are 1, 3, 7, 9.
+        Adjacent keys give you the ones in between.
 
-    Modifiers at the bottom of the file.
+        Any diagonal makes 0.
+        Any 3 with the top row makes 00.
+        L-shape makes 000
+        Backwards L-shape makes ,000
 
-Changes:
-    - Modifiers
+
+    The other keys are available for options:
+
+        F to press the function key instead
+        T (plus others) for writing the time
+
+        See OPTIONS defined below for what's available
 """
+
+from plover.system import Stroke
 
 LONGEST_KEY = 1
 
+# set numbers for -FRPB block
 NUMBERS = {
-    # RHWP
-    0b0001: "1",
-    0b0101: "2",
-    0b0100: "3",
-    0b0011: "4",
-    0b1111: "5",
-    0b1100: "6",
-    0b0010: "7",
-    0b1010: "8",
-    0b1000: "9",
-    0b1001: "0",
-    0b0110: "0",
-    0b0111: "00",
-    0b1011: "000",
-    0b1101: "00",
-    0b1110: "000",
+    "": "",
+    "-F": "1",
+    "-FP": "2",
+    "-P": "3",
+    "-FR": "4",
+    "-FRPB": "5",
+    "-PB": "6",
+    "-R": "7",
+    "-RB": "8",
+    "-B": "9",
+    "-RP": "0",  # diagonals
+    "-FB": "0",
+    "-FRP": "00",  # top left corner
+    "-FPB": "00",  # top right
+    "-FRB": "000",  # bottom left
+    "-RPB": ",000",  # bottom right
 }
 
+# then copy the layout for the -LGTS block
+for key, value in NUMBERS.copy().items():
+    keys = Stroke(key).keys()
+    new_keys = set()
+    for first, second in [("-F", "-L"), ("-P", "-T"), ("-R", "-G"), ("-B", "-S")]:
+        if first in keys:
+            new_keys.add(second)
 
-def function(x):
-    if x == "0" or int(x) > 12:
+    NUMBERS[Stroke(new_keys).rtfcre] = value
+
+
+def function_key(x):
+    if int(x) == 0 or int(x) > 12:
         raise KeyError
     return f"{{#F{x}}}"
+
+
+OPTIONS = {
+    "": lambda x: f"{{&{x}}}",
+    "A": lambda x: "{^}" + x,  # Attach
+    "TPH-": lambda x: "-" + x,  # Negative
+    "TPHA": lambda x: "{^}-" + x,  # Attach Negative
+    "TP-": function_key,  # Function key
+    "EU": lambda x: f"{{#Super({x})}}",  # i3: Go to workspace x
+    "*EU": lambda x: f"{{#Super(Shift({x}))}}",  # i3: Move to workspace x
+    "AO": lambda x: x + "{}",  # separate numbers. AO like Emily spacing
+    "O": lambda x: " ".join(x) + "{}",  # separate digits
+    "T-": lambda x: f"{x}:00",  # Time
+    "TA": lambda x: f"{x}am",  # Time am
+    "TO": lambda x: f"{x}pm",  # Time pm
+    "T*": lambda x: f"{{^}}:{x}",  # Time (minutes)
+    "TA*": lambda x: f"{{^}}:{x}am",  # Time (minutes) am
+    "TO*": lambda x: f"{{^}}:{x}pm",  # Time (minutes) pm
+    "P": lambda x: f"${{^}}{{&{x}}}",  # Pounds
+    "H": lambda x: f"${{^}}{{&{x}}}",  # dollars
+}
 
 
 def lookup(strokes):
     assert len(strokes) <= LONGEST_KEY
 
-    stroke = strokes[0]
-
-    # first translate stroke into a set of keys
-    key_prefix = ""
-    keys = set()
-    for c in stroke:
-        i = "1234506789".find(c)
-        if i > -1:
-            c = "STPHAOFPLT"[i]
-            keys.add("#")
-        if c in "AO-*EU":
-            key_prefix = "-"
-            if c != "-":
-                keys.add(c)
-        else:
-            keys.add(key_prefix + c)
-
+    stroke = Stroke(strokes[0])
+    keys = stroke.keys()
     if "#" not in keys:
         raise KeyError
 
-    groups = [
-        sum((key in keys) << index for index, key in enumerate(group))
-        for group in (
-            ("S", "T", "K", "A", "O", "E", "U", "-D", "-Z"),
-            ("P", "W", "H", "R"),
-            ("-F", "-R", "-P", "-B"),
-            ("-L", "-G", "-T", "-S"),
-        )
-    ]
+    options = stroke & Stroke("STKPWHRAO*EUDZ")
+    digit1 = stroke & Stroke("-FRPB")
+    digit2 = stroke & Stroke("-LGTS")
 
-    if all(x == 0 for x in groups[1:]):
-        raise KeyError
-    while groups[-1] == 0:
-        groups.pop()
-    if any(x == 0 for x in groups[1:]):
+    output = ""
+    output += NUMBERS[digit1.rtfcre]
+    output += NUMBERS[digit2.rtfcre]
+
+    if output == "":
         raise KeyError
 
-    main = "".join(NUMBERS[x] for x in groups[1:])
+    output = OPTIONS[options.rtfcre](output)
 
-    # modifiers:
-    main = {
-        # ZDUEOAKTS
-        0b000000000: lambda x: x,
-        0b000001000: lambda x: "{^}" + x,  # A: Append
-        0b000000001: lambda x: "-" + x,  # S: Minus
-        0b000000010: function,  # T: Function (like in modifiers, TP=F)
-        0b000001001: lambda x: "{^}-" + x,  # SA: Append minus
-        0b000000011: lambda x: x + "{}",  # ST: SeparaTe numbers
-        0b000000111: lambda x: " ".join(x) + "{}",  # SD: Separate Digits
-        0b001100000: lambda x: "{^}[" + x + "]",  # I: Index
-        0b001100001: lambda x: "{^}[-" + x + "]",  # SI: negative index
-        0b000000101: lambda x: f"{{#Super(Shift({x}))}}",  # SK: move window to workspace x (i3)
-        0b000000100: lambda x: f"{{#Super({x})}}",  # K: Change to workspace x (i3)
-        0b000000110: lambda x: x + "{^}:{^}",  # D-: colon, e.g. for time
-        0b010000000: lambda x: x + "am",  # D: am
-        0b100000000: lambda x: x + "pm",  # Z: pm
-    }[groups[0]](main)
-    return main
-
-
-if __name__ == "__main__":
-    from scripts.conflicts import prepare, check_lookup_conflicts
-
-    json_merged, python_lookups = prepare()
-
-    check_lookup_conflicts(json_merged, python_lookups, lookup)
+    return output
