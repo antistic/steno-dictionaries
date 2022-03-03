1f2dddda7d59021eeafd7467f1ebd3d225ca2f43
Antonia Siu <mail@antoniasiu.co.uk>
2022-03-03 21:37:27 +0000

    Add conflict and fixes support

--- fingerspelling/fingerspelling.py
+++ fingerspelling/fingerspelling.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python
-"""Create a fingerspelling JSON dictionary
+"""Generate a fingerspelling dictionary (and optionally find conflicts)
 
 Usage:
     Edit the SPELLING and VARIANTS
 
-    Then run `python fingerspelling`
-    `python fingerspelling.py -h` for help on what the options are
+    Run `python fingerspelling.py -h` for help
+
+Limitations:
+    Won't find conflicts in dictionaries that don't implement `.items()`, such as
+    python dictionaries
 """
 
 import argparse
@@ -92,31 +95,128 @@ def generate_dictionary():
     return dictionary
 
 
+def find_conflicts():
+    from plover.dictionary import base as dictionary
+    from plover.registry import registry
+    from plover import system
+    from plover.config import Config
+    from plover.oslayer.config import CONFIG_FILE
+
+    config = Config(CONFIG_FILE)
+    config.load()
+    registry.update()
+    system.setup("English Stenotype")
+    dicts = [
+        dictionary.load_dictionary(path)
+        for path, enabled in config["dictionaries"]
+        if enabled
+    ]
+
+    conflicts = []
+    fixes = {}
+    for dict in dicts:
+        dict_conflicts = []
+
+        for key, value in dict.items():
+            all_parts_conflict = True
+            any_conflict = False
+            suggestion = ""
+            for stroke in key:
+                if stroke in output.keys():
+                    suggestion += output[stroke]
+                    any_conflict = True
+                else:
+                    all_parts_conflict = False
+                    break
+
+            rtfcre = "/".join(key)
+            if all_parts_conflict:
+                if suggestion != output.get(rtfcre, ""):
+                    fixes[rtfcre] = suggestion
+                    dict_conflicts.append(f"{rtfcre:50}{value:30}{suggestion}")
+            elif any_conflict:
+                dict_conflicts.append(f"{rtfcre:50}{value}")
+
+        if len(dict_conflicts) > 0:
+            conflicts.append("-" * len(dict.path))
+            conflicts.append(dict.path)
+            conflicts.append("-" * len(dict.path))
+            conflicts.append("\n".join(dict_conflicts))
+            conflicts.append("")
+
+    return conflicts, fixes
+
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Generate a fingerspelling dictionary")
+    parser = argparse.ArgumentParser(
+        description="Generate a fingerspelling dictionary (and optionally find conflicts)"
+    )
     parser.add_argument(
         "output_file",
-        help="Dictionary output file (default: fingerspelling.json)",
+        help="File to save the dictionary to (default: fingerspelling.json)",
         type=str,
         nargs="?",
         default="fingerspelling.json",
     )
     parser.add_argument(
-        "--console",
+        "--print-dictionary",
         help="Print dictionary to the console instead of saving it to a file",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--conflicts",
+        help=(
+            "Find conflicts with your active dictionaries and saves the results to files. "
+            "It will find if any stroke of a definition conflicts, and if all parts "
+            "conflict, it will suggest a fix."
+        ),
+        action="store_true",
+    )
+    parser.add_argument(
+        "--conflicts-file",
+        help="File to save conflicts to (default: conflicts.txt)",
+        type=str,
+        default="conflicts.txt",
+    )
+    parser.add_argument(
+        "--fixes-file",
+        help="File to save conflict fixes suggestions to (default: fixes.json)",
+        type=str,
+        default="fixes.json",
+    )
+    parser.add_argument(
+        "--print-conflicts",
+        help="Print conflicts and fixes instead of saving to files",
         action="store_true",
     )
 
     args = parser.parse_args()
 
-    dictionary = generate_dictionary()
+    output = generate_dictionary()
 
-    if args.console:
-        print(json.dumps(dictionary, indent=2))
+    if args.conflicts or args.print_conflicts:
+        conflicts, fixes = find_conflicts()
+
+        if args.print_conflicts:
+            print("CONFLICTS\n")
+            print("\n".join(conflicts))
+        else:
+            conflicts_file = Path(args.conflicts_file)
+            conflicts_file.write_text("\n".join(conflicts))
+            print(f"Wrote conflicts to {conflicts_file.resolve()}")
+
+            fixes_file = Path(args.fixes_file)
+            fixes_file.write_text(json.dumps(fixes, indent=4))
+            print(f"Wrote fixes to {fixes_file.resolve()}")
+
+    if args.print_dictionary:
+        print("DICTIONARY\n")
+        print(json.dumps(output, indent=2))
+        print(f"\n{len(output.keys())} entries")
     else:
         output_path = Path(args.output_file)
-        output_path.write_text(json.dumps(dictionary, indent=2))
+        output_path.write_text(json.dumps(output, indent=2))
 
         print(
-            f"Wrote dictionary ({len(dictionary.keys())} entries) to {output_path.resolve()}"
+            f"Wrote dictionary ({len(output.keys())} entries) to {output_path.resolve()}"
         )
